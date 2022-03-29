df47e0489d7e6b1de23369183ef9b12246efbdd6
karan <k.zala5656@gmail.com>
2022-03-29 11:51:49 +0530

    Combined all html in one file

--- arrayIntroduction/script.js
+++ arrayIntroduction/script.js
@@ -1,58 +1,16 @@
-var pages = {
-    'string': 'String page',
-    'lastElement': 'Last Element page',
-    'push': 'Push page'
-}
+function getPageContent(page){
+    const targetDiv = document.getElementById(page)
+    let childLength = document.getElementById('content').children.length
 
-function loadPage(href) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", href, false);
-    xmlhttp.send();
-    return xmlhttp.responseText;
-}
-
-function getPageContent(page) {
-    var content
-    switch (page) {
-        case 'string':
-            content = loadPage('string.html')
-            break;
-
-        case 'lastElement':
-            content = loadPage('last-element.html')
-            break;
-
-        case 'push':
-            content = loadPage('push.html')
-            break;
-
-        case 'pop':
-            content = loadPage('pop.html')
-            break;
-
-        case 'unshift':
-            content = loadPage('unshift.html')
-            break;
-
-        case 'splice':
-            content = loadPage('splice.html')
-            break;
-
-        case 'shift':
-            content = loadPage('shift.html')
-            break;
-
-        case 'sort':
-            content = loadPage('sort.html')
-            break;
-
-        case 'reverse':
-            content = loadPage('reverse.html')
-            break;
-
-        default:
-            content = loadPage('string.html')
-            break;
+    for(i=3;i<childLength*4;i=i+4){
+       if(document.getElementById('content').childNodes[i].style.display === 'block'){
+        document.getElementById('content').childNodes[i].style.display = 'none'
+      }
     }
-    document.getElementById('content').innerHTML = content
-}
+
+    if (targetDiv.style.display !== "none") {
+      targetDiv.style.display = "none";
+    } else {
+      targetDiv.style.display = "block";
+    }
+  }
